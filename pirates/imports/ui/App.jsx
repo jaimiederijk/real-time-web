e327bff792b4f7c07de12d7acd0e5b598c610df2
jaimiederijk <j.r.derijk@quicknet.nl>
2016-04-12 13:00:03 +0100

    react basis

---
+++ pirates/imports/ui/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component,PropTypes } from 'react';
+
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.jsx';
+
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+
+// App component - represents the whole app
+class App extends Component {
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <AccountsUIWrapper />
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  return {
+    tasks:Tasks.find({}).fetch()
+  };
+},App);
